3b4f5ca8ec2b421232e440ba4272f855c76a104b
Yinon Levin <yinonlevin@yinons-MacBook-Pro-2.local>
2025-02-22 23:41:18 +0200

    Add Railway deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     domains: ['image.tmdb.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
