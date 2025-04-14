d41922281f76b8035e25d98ff911a5388a57793f
barleviatias <barlevi.atias@gmail.com>
2025-04-14 15:52:44 +0300

    feat: implement manual pageview tracking with PostHog in new PostHogPageView component

--- app/posthogProvider.tsx
+++ app/posthogProvider.tsx
@@ -1,26 +1,26 @@
+// app/providers.tsx
 'use client';
 
 import posthog from 'posthog-js';
 import { PostHogProvider as PHProvider } from 'posthog-js/react';
 import { useEffect } from 'react';
+import dynamic from 'next/dynamic';
 
+const PostHogPageView = dynamic(() => import('./PostHogPageView'), {
+	ssr: false,
+});
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
 	useEffect(() => {
-		if (
-			!process.env.NEXT_PUBLIC_POSTHOG_KEY ||
-			!process.env.NEXT_PUBLIC_POSTHOG_HOST
-		) {
-			console.error(
-				'PostHog environment variables are not properly configured'
-			);
-			return;
-		}
-
-		posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+		posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
 			api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
 			capture_pageview: false, // Disable automatic pageview capture, as we capture manually
 		});
 	}, []);
 
-	return <PHProvider client={posthog}>{children}</PHProvider>;
+	return (
+		<PHProvider client={posthog}>
+			<PostHogPageView />
+			{children}
+		</PHProvider>
+	);
 }
